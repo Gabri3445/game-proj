f880771377080446c279d70d571b6b007f02bd6a
gabri3445 <gabriele.ope@gmail.com>
2025-07-17 13:41:05 +0200

    Camera follow script

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    private void Start()
+    {
+        if (target == null)
+        {
+            Debug.LogError("No target assigned");
+            return;
+        }
+        transform.position = target.position + offset;
+    }
+
+    private void LateUpdate()
+    {
+        if (target)
+        {
+            transform.position = target.position + offset;
+        }
+    }
+}
